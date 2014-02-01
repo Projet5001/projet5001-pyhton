6f61c8583ca4157bbf19afef4eec2559edfd4360
plperron <plperron@plperron-laptop.(none)>
2014-01-31 21:25:42 -0500

    Ajout du déplacement du personnage via les flèches du clavier

--- main.py
+++ main.py
@@ -2,15 +2,31 @@
 import pygame
 import tmx
 
-class Perso(pygame.sprite.Sprite):
+class UserInput:
+    def __init__(self, perso):
+        self.perso = perso
 
+    def update(self):
+        # -------- scroll the big map ----------
+        pressedkeys = pygame.key.get_pressed()
+        if pressedkeys[pygame.K_LEFT]:
+             self.perso.position['x'] -= 1
+        if pressedkeys[pygame.K_RIGHT]:
+             self.perso.position['x'] += 1
+        if pressedkeys[pygame.K_UP]:
+             self.perso.position['y'] -= 1
+        if pressedkeys[pygame.K_DOWN]:
+             self.perso.position['y'] += 1
+
+class Perso(pygame.sprite.Sprite):
     def __init__(self, screen, image):
         super(Perso, self).__init__()
         self.screen = screen
         self.image = pygame.image.load(image)
+        self.position = {'x': 0, 'y': 0}
 
     def update(self):
-        self.screen.blit(self.image, (0, 0))
+        self.screen.blit(self.image, (self.position['x'], self.position['y']))
 
 
 def main():
@@ -21,6 +37,7 @@ def main():
     clock = pygame.time.Clock()
     dt = clock.tick(30)
     perso = Perso(screen, "perso.png")
+    userInput = UserInput(perso)
 
     while True:
         for event in pygame.event.get():
@@ -32,6 +49,7 @@ def main():
         screen.fill(black)
         # Draw all layers of the tilemap to the screen.
         tilemap.draw(screen)
+        userInput.update()
         perso.update()
         # Refresh the display window.
         pygame.display.flip()
