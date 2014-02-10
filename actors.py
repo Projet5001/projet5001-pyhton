2f943870060820d560ed8cebd8732d3a0adbe67d
Alexandre Leblanc <info@alexandre-leblanc.com>
2014-02-09 23:11:51 -0500

    some fix....

--- actors.py
+++ actors.py
@@ -3,14 +3,26 @@
 import pygame
 
 class Actor(pygame.sprite.Sprite):
-    def __init__(self, screen, image, position, *groups ):
+    def __init__(self, image, position, *groups):
         super(Actor, self).__init__(*groups)
-        self.screen = screen
         self.image = pygame.image.load(image)
         self.rect = pygame.rect.Rect(position, self.image.get_size())
+        self.savedLastPos = (self.rect.x, self.rect.y)
+
+        #spec of perso
+        self.speed = 10
+        self.accel = 1
+        self.isDoing = "nothing"
+
+    def saveLastPos(self):
+        self.savedLastPos = (self.rect.x, self.rect.y)
+
+    def resetPos(self):
+        self.rect.x, self.rect.y = self.savedLastPos
 
     def update_position(self):
         pass
 
+    # this need the dt and game otherwise bug in tmx
     def update(self, dt, game):
         game.tilemap.set_focus(self.rect.x, self.rect.y)
