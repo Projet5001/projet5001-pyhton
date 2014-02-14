4ca16b3ed42bef566d93fe3a7f60caf55b7b9cd2
Mathieu Trudel-Lapierre <trudel-lapierre.mathieu@courrier.uqam.ca>
2014-02-13 22:48:00 -0500

    userInput: corriger les collisions "diagonales" qui permettaient de marcher à travers les objets
    Signed-off-by: Mathieu Trudel-Lapierre <trudel-lapierre.mathieu@courrier.uqam.ca>

--- userInput.py
+++ userInput.py
@@ -9,19 +9,15 @@ class Keyboard():
         self.accel = perso.accel
 
     def move_left(self):
-        self.perso.saveLastPos()
         self.perso.rect.x -= (self.speed * self.accel)
 
     def move_right(self):
-        self.perso.saveLastPos()
         self.perso.rect.x += (self.speed * self.accel)
 
     def move_up(self):
-        self.perso.saveLastPos()
         self.perso.rect.y -= (self.speed * self.accel)
 
     def move_down(self):
-        self.perso.saveLastPos()
         self.perso.rect.y += (self.speed * self.accel)
 
     def jump(self):
@@ -29,6 +25,8 @@ class Keyboard():
 
     def updateKey(self, dt):
         pressedkeys = pygame.key.get_pressed()
+        self.perso.saveLastPos()
+
         if pressedkeys[pygame.K_LEFT]:
             self.move_left()
 
@@ -40,6 +38,7 @@ class Keyboard():
 
         if pressedkeys[pygame.K_DOWN]:
             self.move_down()
+
         if pressedkeys[pygame.K_SPACE]:
             self.jump()
 
