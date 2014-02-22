ae21e3aec397bcc5572a38ada61d952734cc69fc
alexandre <you@example.com>
2014-02-22 16:11:36 -0500

    ajout de la class monstre et methode qui ajoute les monstre

---
+++ monster.py
@@ -0,0 +1,11 @@
+import actors
+
+
+class Monster(actors.Actor):
+    def __init__(self, image, position, *groups):
+        super(Monster, self).__init__(image, position, *groups)
+
+
+    def update(self, dt, game):
+        pass
+
