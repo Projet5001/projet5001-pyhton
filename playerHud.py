a31ad2f141ccef8293be59cc0ba1855c8f8c9c1f
Pierre-Luc <pl.perron@hotmail.com>
2014-03-01 21:36:55 -0500

    FIX: Le Hud suit le personnage même dans les coins

--- playerHud.py
+++ playerHud.py
@@ -4,10 +4,17 @@ import pygame
 from lib import tmx
 
 class PlayerHud(tmx.Layer):
-    def __init__(self, name, player):
+    def __init__(self, name, player, game):
+        """
+
+        :param name: Le nom du hub (permet la recherche dans layers.__get_name__())
+        :param player: Le joueur auquelle nous voulons afficher l'information
+        :param screen: Permet de récupérer (width, height) de l'écran dynamiquement
+        """
         self.name = name
         self.visible = True
         self.player = player
+        self.game = game
 
     def __iter__(self):
         return tmx.LayerIterator(self)
@@ -18,4 +25,23 @@ class PlayerHud(tmx.Layer):
     def draw(self, surface):
         myfont = pygame.font.SysFont("monospace", 15)
         label = myfont.render(self.player.name, 1, (255,255,255))
-        surface.blit(label, (self.player.collision_rect.x, self.player.collision_rect.y))
+        #Centrer la position du HUD par rapport au personnage
+        playerx, playery = self.player.collision_rect.x, self.player.collision_rect.y
+        hubx = playerx - (playerx - (self.game.screen.get_width() / 2))
+        huby = playery - (playery - (self.game.screen.get_height() / 2))
+        #Gérer les bordures
+        if(playerx  < hubx):
+            hubx = playerx
+        if(playery  < huby):
+            huby = playery
+        centerx = self.game.tilemap.px_width - self.game.screen.get_width() / 2
+        centery = self.game.tilemap.px_height - self.game.screen.get_height() / 2
+        if(playerx > centerx):
+            hubx += playerx - centerx
+        if(playery > centery):
+            huby += playery - centery
+        print "hubx" + str(hubx) + " <=> playerx " + str(playerx)
+        print "huby" + str(huby) + " <=> playery " + str(playery)
+        surface.blit(label, (hubx, huby))
+
+
