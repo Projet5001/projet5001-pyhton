a083a4822a4c72ad56047abbc18cab3f727d505a
Mathieu Trudel-Lapierre <mathieu.trudel-lapierre@canonical.com>
2014-03-03 18:35:52 -0500

    Plus de corrections de styles pour PEP-8, espacement et longueur de ligne.
    Signed-off-by: Mathieu Trudel-Lapierre <mathieu.trudel-lapierre@canonical.com>

--- playerHud.py
+++ playerHud.py
@@ -3,13 +3,14 @@ import pygame
 
 from lib import tmx
 
+
 class PlayerHud(tmx.Layer):
     def __init__(self, name, player, screen, tilemap):
         """
 
-        :param name: Le nom du hub (permet la recherche dans layers.__get_name__())
+        :param name: Le nom du HUD
         :param player: Le joueur auquelle nous voulons afficher l'information
-        :param screen: Permet de récupérer (width, height) de l'écran dynamiquement
+        :param screen: Écran pour le dessin des éléments du HUD
         """
         self.name = name
         self.visible = False
@@ -20,8 +21,8 @@ class PlayerHud(tmx.Layer):
     def move(self, offsetx, offsety):
         pass
 
-    def setVisible(self, bool):
-        self.visible = bool
+    def setVisible(self, visible):
+        self.visible = visible
 
     def __iter__(self):
         return tmx.LayerIterator(self)
@@ -30,35 +31,52 @@ class PlayerHud(tmx.Layer):
         pass
 
     def draw(self, surface):
-        hub = self.__followPlayer() #Tous les autres objets seront placés en relation avec ce point
-        pygame.draw.line(surface, (0, 0, 0), (hub["x"], hub["y"]), (hub["x"] + 1, hub["y"]), 1)
+        # Tous les autres objets seront placés en relation avec ce point
+        hub = self.__followPlayer()
+        pygame.draw.line(surface,
+                         (0, 0, 0),
+                         (hub["x"], hub["y"]),
+                         (hub["x"] + 1, hub["y"]),
+                         1)
         hub["y"] -= self.tilemap.tile_height
         self.__showName(surface, (hub["x"], hub["y"]))
         self.__showHealth(surface, (hub["x"], hub["y"] + 5))
 
     def __showName(self, surface, hub):
         myfont = pygame.font.SysFont("monospace", 15, True)
-        label = myfont.render(self.player.name, 1, (255,255,255))
-        surface.blit(label, ((hub[0] - (label.get_width() / 2), hub[1] - (self.tilemap.tile_height))))
+        label = myfont.render(self.player.name, 1, (255, 255, 255))
+        surface.blit(label, (hub[0] - (label.get_width() / 2),
+                             hub[1] - (self.tilemap.tile_height)))
 
     def __showHealth(self, surface, hub):
         #line(Surface, color, start_pos, end_pos, width=1) -> Rect
         tileHalfWidth = self.tilemap.tile_width / 2
         originx = hub[0] - tileHalfWidth
-        originy = hub[1] -  self.tilemap.tile_height / 2
-        pygame.draw.line(surface, (0, 0, 0), (originx, originy), (originx + tileHalfWidth * 2, originy), 5)
-        health = (tileHalfWidth * 2)  * (float(self.player.health["hp"]) / self.player.health["max"])
-        pygame.draw.line(surface, (255, 0, 0), (originx, originy), (originx + health, originy), 3)
+        originy = hub[1] - self.tilemap.tile_height / 2
+        pygame.draw.line(surface,
+                         (0, 0, 0),
+                         (originx, originy),
+                         (originx + tileHalfWidth * 2, originy),
+                         5)
+        health = (tileHalfWidth * 2) \
+                    * float(self.player.health["hp"] \
+                            / self.player.health["max"])
+        pygame.draw.line(surface,
+                         (255, 0, 0),
+                         (originx, originy),
+                         (originx + health, originy),
+                         3)
 
     def __followPlayer(self):
         #Centrer la position du HUD par rapport au personnage
-        playerx, playery = self.player.collision_rect.x, self.player.collision_rect.y
+        playerx = self.player.collision_rect.x
+        playery = self.player.collision_rect.y
         hubx = playerx - (playerx - (self.screen.get_width() / 2))
         huby = playery - (playery - (self.screen.get_height() / 2))
         #Gérer les bordures
-        if(playerx  < hubx):
+        if(playerx < hubx):
             hubx = playerx
-        if(playery  < huby):
+        if(playery < huby):
             huby = playery
         centerx = self.tilemap.px_width - self.screen.get_width() / 2
         centery = self.tilemap.px_height - self.screen.get_height() / 2
@@ -68,5 +86,4 @@ class PlayerHud(tmx.Layer):
             huby += playery - centery
         hubx += self.tilemap.tile_width / 2
         huby += self.tilemap.tile_height / 2
-        return {"x": hubx, "y":huby}
-
+        return {"x": hubx, "y": huby}
