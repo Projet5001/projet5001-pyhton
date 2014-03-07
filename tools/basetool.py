1d1434f107d6b1ceaa2bb4c1b718aef8326d5ad9
Mathieu Trudel-Lapierre <mathieu.tl@gmail.com>
2014-03-06 23:51:25 -0500

    tools: déplacement des tools dans un répertoire, classe parente renommée en BaseTool
    Signed-off-by: Mathieu Trudel-Lapierre <mathieu.tl@gmail.com>

---
+++ tools/basetool.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+import pygame
+import importlib
+import tools
+
+
+class BaseTool(pygame.sprite.Sprite):
+    def __init__(self, game, player, name, obj=None):
+        super(BaseTool, self).__init__()
+        self.game = game
+        self.name = name
+        self.player = player
+        self.visible = False
+        self.tmx_object = obj
+
+    def definir_position(self, x, y):
+        pass
+
+    def update(self, dt, *args):
+        pass
+
+    def handle_collision(self):
+        pass
+
+    @classmethod
+    def make_tool(classe, game, player, tmx_object=None):
+        classe.__load_subclasses()
+        for tool_class in classe.__subclasses__():
+            if tmx_object and tool_class.is_type_for(tmx_object.type):
+                return tool_class(game, player, tmx_object.name, tmx_object)
+
+    '''
+    Import dynamically all the "subclasses" of Tool from the directory
+    '''
+    @classmethod
+    def __load_subclasses(classe):
+        for module in tools.__all__:
+            importlib.import_module(".".join(["tools", module]))
+
