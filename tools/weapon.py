499e4fb45af7e5b7081005b5764001363958d79e
Mathieu Trudel-Lapierre <mathieu.tl@gmail.com>
2014-03-19 18:47:57 -0400

    Ajout header pour copyright, encodage, et tri des import

--- tools/weapon.py
+++ tools/weapon.py
@@ -1,7 +1,28 @@
+# -*- coding: utf-8 -*-
+#
+# Projet5001: un jeu de survie post-apocalyptique
+# Copyright (C) 2014  Équipe Projet5001
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 import pygame
-from pygame.locals import *
-from basetool import BaseTool
 from pygame import Rect
+from pygame.locals import *
+
+from basetool import BaseTool
+
 
 class Weapon(BaseTool):
     def __init__(self, layer_manager, player, name, obj=None):
