0125020e1fb22eec64658e53210ccd9986939e54
Mathieu Trudel-Lapierre <mathieu.tl@gmail.com>
2014-04-16 14:43:30 -0400

    Fix scope for monster damage.
    Signed-off-by: Mathieu Trudel-Lapierre <mathieu.tl@gmail.com>

--- collisionManager.py
+++ collisionManager.py
@@ -71,9 +71,9 @@ class CollisionManager():
             for s in self.layer_manager['npcs']:
                 if monstre.collision_rect.colliderect(s.collision_rect):
                     monstre.collision_events.append(s)
-            if monstre.collision_rect.colliderect(self.player.collision_rect):
-                monstre.resetPos()
-                self.player.take_dommage(s.calcul_dommage())
+                if monstre.collision_rect.colliderect(self.player.collision_rect):
+                    monstre.resetPos()
+                    self.player.take_dommage(s.calcul_dommage())
                 
     def monster_manageCollisionEvents(self):
 
